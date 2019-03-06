f51fbedbf987d75cc87d7ee05669ad0f39e3286b
Anthony Beaumont <dev.xan105@outlook.be>
2019-03-06 13:11:53 +0700

    init

---
+++ test.js
@@ -0,0 +1,61 @@
+const tasklist = require('./tasklist.js');
+
+tasklist().then((list)=>{
+
+  console.log("- List All");
+  console.log(list);
+  console.log("\n");
+
+}).catch((err)=>{
+  console.error(err);
+});
+
+tasklist({verbose: true}).then((list)=>{
+
+  console.log("- List All (Verbose)");
+  console.log(list);
+  console.log("\n");
+
+}).catch((err)=>{
+  console.error(err);
+});
+
+tasklist.getProcessInfo("explorer.exe",{verbose: true}).then((process)=>{
+
+  console.log("- Get explorer.exe info (Verbose)");
+  console.log(process);
+  console.log("\n");
+
+}).catch((err)=>{
+  console.error(err);
+});
+
+tasklist.isProcessRunning("firefox.exe").then((bool)=>{
+
+  console.log("- is firefox.exe running ?");
+  console.log(bool);
+  console.log("\n");
+
+}).catch((err)=>{
+  console.error(err);
+});
+
+tasklist.hasProcess("system idle process").then((bool)=>{
+
+  console.log("- is system idle process loaded ?");
+  console.log(bool);
+  console.log("\n");
+
+}).catch((err)=>{
+  console.error(err);
+});
+
+tasklist({uwpOnly: true}).then((list)=>{
+
+  console.log("- List All WinStore App");
+  console.log(list);
+  console.log("\n");
+
+}).catch((err)=>{
+  console.error(err);
+});
